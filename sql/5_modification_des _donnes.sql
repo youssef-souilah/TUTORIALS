802a8c8f120474d46185004c8ae2a6ab51a389cb
Youssef Souilah <souilah.youssef.0@gmail.com>
2025-01-26 12:50:03 +0100

    UPDATE && DELETE

--- sql/5_modification_des _donnes.sql
+++ sql/5_modification_des _donnes.sql
@@ -4,15 +4,15 @@ SET column1 = value1, column2 = value2, ...
 WHERE condition;
 -- exemple
 UPDATE Customers
-SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
+SET ContactName = 'hamid', City= 'rabat'
 WHERE CustomerID = 1;
 -- exemple de modification des plusieurs lignes
 UPDATE Customers
-SET ContactName='Juan';
+SET ContactName='taha';
 
 -- utilisant DELETE
 DELETE FROM nom_table WHERE condition;
 -- Exemple
-DELETE FROM Customers WHERE CustomerName='Alfreds Futterkiste';
+DELETE FROM Customers WHERE CustomerName='hamid';
 -- supprimer tous les lignes
 DELETE FROM Customers;
