1c0ed2b5e51acc017e516525e58f602e646bfc39
Youssef Souilah <souilah.youssef.0@gmail.com>
2025-01-26 12:32:53 +0100

    select

---
+++ sql/4_select.sql
@@ -0,0 +1,35 @@
+-- syntax 
+SELECT column1, column2, ...
+FROM nom_table;
+
+-- Exemple 
+SELECT CustomerName, City FROM Customers;
+--  affichage de tous les cols
+SELECT * FROM Customers;
+
+-- afficher les resultat sans duplication
+-- le mot clés  DISTINCT
+SELECT DISTINCT Country FROM Customers;
+-- filtrer les resultat  utilisant  WHERE
+SELECT * FROM Customers
+WHERE Country='Mexico';
+-- exemple utilisant AND et OR
+SELECT * FROM Customers
+WHERE Country = 'Spain' AND (CustomerName LIKE 'G%' OR CustomerName LIKE 'R%'); 
+-- exemple utilisant NOT
+SELECT * FROM Customers
+WHERE NOT Country = 'Spain';
+-- Syntax de l'ordre
+SELECT column1, column2, ...
+FROM table_name
+ORDER BY column1, column2, ... ASC|DESC;
+-- exemple
+SELECT * FROM Products
+ORDER BY Price DESC;
+-- filtrage utilisant IS NULL et IS NOT NULL
+SELECT CustomerName, ContactName, Address
+FROM Customers
+WHERE Address IS NOT NULL;
+-- limiter les resultats
+SELECT * FROM Customers
+LIMIT 3;
