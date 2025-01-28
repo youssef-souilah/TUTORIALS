986afe3b0cacd82d7f3315ec3736aa9679c7b104
Youssef Souilah <souilah.youssef.0@gmail.com>
2025-01-28 13:03:12 +0100

    IN - BETWEEN

---
+++ 6_in_and_between.sql
@@ -0,0 +1,32 @@
+-- utilisation IN
+SELECT column
+FROM nom_table
+WHERE column IN (value1, value2, ...);
+
+--Exemple
+SELECT * FROM Customers
+WHERE Country IN ('Germany', 'France', 'UK');
+
+--Exemple utilisant NOT
+SELECT * FROM Customers
+WHERE Country NOT IN ('Germany', 'France', 'UK');
+
+--utilisation IN  avec des requetes SELECT
+SELECT column(s)
+FROM nom_table
+WHERE column IN (SELECT column FROM table2 );
+
+-- utilisant BETWEEN
+SELECT column
+FROM nom_table
+WHERE column BETWEEN value1 AND value2;
+-- Exemple
+SELECT * FROM Products
+WHERE Price BETWEEN 10 AND 20;
+-- Exemple utilisant NOT
+SELECT * FROM Products
+WHERE Price NOT BETWEEN 10 AND 20;
+--
+SELECT * FROM Orders
+WHERE OrderDate BETWEEN '1996-07-01' AND '1996-07-31';
+
